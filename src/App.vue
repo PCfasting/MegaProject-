8db08cc348fec462584bb56a62668e4b4b0f3006
vesclop <vesclop2.0@gmail.com>
2021-02-04 16:31:13 +0300

    nav is good

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer 
+      app
+      dark
+      color="green"
+      hide-overlay
+      permanent
+      right
+      expand-on-hover
+    >
+    <v-list-item two-line>
+      <v-list-item-avatar>
+        <img :src="'https://randomuser.me/api/portraits/men/83.jpg'">
+      </v-list-item-avatar>
+      <v-list-item-content class="text-left">
+        <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+        <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider class="my-3" />
+
+    <v-list nav shaped dense>
+      <v-list-item link >
+        <v-list-item-icon>
+          <v-icon>mdi-home-outline</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-left">Главная</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item link >
+        <v-list-item-icon>
+          <v-icon>mdi-account</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-left">Мой профиль</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item link >
+        <v-list-item-icon>
+          <v-icon>mdi-account-plus-outline</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-left">Найти друзей</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    </v-navigation-drawer>
+
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
